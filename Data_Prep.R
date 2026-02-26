57b3280940c9c2370e6c397d657814be7a70f9ce
drnanaquaicoe <faracaven@gmail.com>
2026-02-26 16:42:44 +0000

    Update Data_Prep.R

--- Data_Prep.R
+++ Data_Prep.R
@@ -1,1 +1,18 @@
 #Data Prep#
+#By Dr. Nana Quaicoe adapted from Harvardx Data Science Certificate#
+
+####Loading the MovieLens Dataset####
+library(tidyverse)
+library(dslabs)
+data("movielens")
+
+####Examine the Data####
+movielens|>as_tibble()
+
+
+####Examine the Number of Unique Users Rating a Movie####
+movielens|>summarize(n_users=n_distinct(userId), n_movies=n_distinct(movieId))
+
+####Consider only movies rated 5 times or more and users that have rated more than 100 of these movies####
+set.seed(2006)
+indexes<-split(1:nrow(movielens), movielens$userId) ###
