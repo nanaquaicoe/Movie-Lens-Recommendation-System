ef026b47728a73c76684a3a41b3c600c59fd708f
drnanaquaicoe <faracaven@gmail.com>
2026-02-26 16:59:32 +0000

    Update Data_Prep.R

--- Data_Prep.R
+++ Data_Prep.R
@@ -18,5 +18,18 @@ set.seed(2006)
 indexes<-split(1:nrow(movielens), movielens$userId)###split data according to userid###
 test_ind <- sapply(indexes, function(ind) sample(ind, ceiling(length(ind)*.2))) |>
   unlist(use.names = TRUE) |> sort() ###for each user it selects 20% of their ratings###
-train_set <- movielens[-test_ind,].  #creating the train set###
-test_set <- movielens[test_ind,].    ###creating the test set###
+train_set <- movielens[-test_ind,]  #creating the train set###
+test_set <- movielens[test_ind,]  ###creating the test set###
+
+
+###remove entries using the semi_join function##
+test_set<- test_set|>semi_join(train_set, by="movieId")
+train_set<-train_set|>semi_join(test_set, by="movieId")
+
+#####Use pivot_wider to make a matric with users represented by rows and movies by columns####
+y <- select(train_set, movieId, userId, rating) |>
+  pivot_wider(names_from = movieId, values_from = rating) 
+rnames <- y$userId
+y <- as.matrix(y[,-1])
+rownames(y) <- rnames
+movie_map <- train_set |> select(movieId, title) |> distinct(movieId, .keep_all = TRUE)
