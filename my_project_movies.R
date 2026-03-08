0eba8793fecc500bbd16e07a092fac53279b2308
drnanaquaicoe <faracaven@gmail.com>
2026-03-08 11:48:16 +0000

    Update my_project_movies.R

--- my_project_movies.R
+++ my_project_movies.R
@@ -1,3 +1,10 @@
-###Creating a Movie Recommendation System####
+##########Creating a Movie Recommendation System##########
 ######### developed by Dr. Nana Quaicoe ########
 #######modified under the Harvardx Data Science Capstone Project######
+############ Dataset used is the 10M version of the MovieLens dataset######
+
+##########################################################
+# Create edx and final_holdout_test sets 
+##########################################################
+
+# Note: this process could take a couple of minutes
